0ba403b46c9c905c85bf9cbf4b2b902e2f44ac57
Alessio Coser <alessio.coser@xpeppers.com>
2018-01-09 16:36:28 +0100

    returns empty array when loggedUser is not a friend

--- javascript/test/TripServiceSpec.js
+++ javascript/test/TripServiceSpec.js
@@ -1,12 +1,22 @@
 'use strict'
 
-let assert = require('assert')
-let TripService = require('../src/TripService')
+const assert = require('assert')
+const TripService = require('../src/TripService')
+
+let loggedUser = { name: 'Logged User' }
+let anotherUser = { name: 'Another User' }
 
 describe('TripService', () => {
   it('throws an error if user is not logged in', () => {
     let tripService = new TripService(null)
 
     assert.throws(() => tripService.getTripsByUser(null), /User not logged in./)
+  })
+
+  it('returns empty array when loggedUser is not a friend', () => {
+    let sampleUser = { name: 'Sample User', getFriends: () => [anotherUser] }
+    let tripService = new TripService(loggedUser)
+
+    assert.deepEqual([], tripService.getTripsByUser(sampleUser))
   })
 })
