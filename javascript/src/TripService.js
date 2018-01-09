50062cd76cf3de4b11109d0d3977f01d6e65115c
Alessio Coser <alessio.coser@xpeppers.com>
2018-01-09 16:47:32 +0100

    invert loggedUser check

--- javascript/src/TripService.js
+++ javascript/src/TripService.js
@@ -10,24 +10,24 @@ class TripService {
   }
 
   getTripsByUser (user) {
-    let tripList = []
-    let isFriend = false
-    if (this.loggedUser != null) {
-      let friends = user.getFriends()
-      for (let i = 0; i < friends.length; i++) {
-        let friend = friends[i]
-        if (friend == this.loggedUser) {
-          isFriend = true
-          break
-        }
-      }
-      if (isFriend) {
-        tripList = this.tripDAO.findTripsByUser(user)
-      }
-      return tripList
-    } else {
+    if (this.loggedUser == null) {
       throw new Error('User not logged in.')
     }
+
+    let tripList = []
+    let isFriend = false
+    let friends = user.getFriends()
+    for (let i = 0; i < friends.length; i++) {
+      let friend = friends[i]
+      if (friend == this.loggedUser) {
+        isFriend = true
+        break
+      }
+    }
+    if (isFriend) {
+      tripList = this.tripDAO.findTripsByUser(user)
+    }
+    return tripList
   }
 }
 
