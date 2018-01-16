92fec6793be3baaf1efa0c897cbd5c81e73a802d
Alessio Coser <alessio.coser@xpeppers.com>
2018-01-16 12:39:14 +0100

    tests returns_empty_list_if_loggedUser_and_user_are_not_friends, refactor tests

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -12,7 +12,8 @@ import static junit.framework.Assert.assertEquals;
 public class TripServiceTest {
 
     private User notLoggedUser = null;
-
+    private User anUser = new User();
+    private User aLoggedUser = new User();
 
     @Test(expected = UserNotLoggedInException.class)
     public void thows_a_not_logged_in_exception() throws Exception {
@@ -23,13 +24,28 @@ public class TripServiceTest {
 
     @Test
     public void returns_empty_list_if_user_has_no_friends() throws Exception {
-        User aLoggedUser = new User();
         TripService tripService = new TripService(aLoggedUser);
 
-        User anUser = new User();
         List<Trip> tripsByUser = tripService.getTripsByUser(anUser);
 
         List<Trip> emptyTripList = new ArrayList<Trip>();
         assertEquals(emptyTripList, tripsByUser);
+    }
+
+    @Test
+    public void returns_empty_list_if_loggedUser_and_user_are_not_friends() throws Exception {
+        TripService tripService = new TripService(aLoggedUser);
+
+        User userWithFriends = anUserWithFriends();
+        List<Trip> tripsByUser = tripService.getTripsByUser(userWithFriends);
+
+        List<Trip> emptyTripList = new ArrayList<Trip>();
+        assertEquals(emptyTripList, tripsByUser);
+    }
+
+    private User anUserWithFriends() {
+        User user = new User();
+        user.addFriend(new User());
+        return user;
     }
 }
