7db077cf4e19abeabbf6f1d741552b1154629c35
Alessio Coser <alessio.coser@xpeppers.com>
2018-01-16 17:16:39 +0100

    refactor naming of test variables and methods

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -3,52 +3,50 @@ package org.craftedsw.tripservicekata.trip;
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
 import org.junit.Test;
-
-import java.util.ArrayList;
 import java.util.List;
 
 import static junit.framework.Assert.assertEquals;
 
 public class TripServiceTest {
 
-    private User notLoggedUser = null;
-    private User aLoggedUser = new User();
-    private User anUser = new User();
+    private static User GUEST = null;
+    private static User LOGGED_USER = new User();
+    private static User ANOTHER_USER = new User();
 
     @Test(expected = UserNotLoggedInException.class)
     public void thows_a_not_logged_in_exception() throws Exception {
-        TripService tripService = new TripService(notLoggedUser);
+        TripService tripService = new TestableTripService(GUEST);
 
         tripService.getTripsByUser(null);
     }
 
     @Test
     public void returns_empty_list_if_user_has_no_friends() throws Exception {
-        TripService tripService = new TripService(aLoggedUser);
+        TripService tripService = new TestableTripService(LOGGED_USER);
 
-        List<Trip> tripsByUser = tripService.getTripsByUser(anUser);
+        List<Trip> friendTrips = tripService.getTripsByUser(ANOTHER_USER);
 
-        assertEquals(0, tripsByUser.size());
+        assertEquals(0, friendTrips.size());
     }
 
     @Test
     public void returns_empty_list_if_loggedUser_and_user_are_not_friends() throws Exception {
-        TripService tripService = new TripService(aLoggedUser);
+        TripService tripService = new TestableTripService(LOGGED_USER);
 
-        User userWithFriends = anUserWithFriends();
-        List<Trip> tripsByUser = tripService.getTripsByUser(userWithFriends);
+        User friend = anUserWithFriends();
+        List<Trip> friendTrips = tripService.getTripsByUser(friend);
 
-        assertEquals(0, tripsByUser.size());
+        assertEquals(0, friendTrips.size());
     }
 
     @Test
     public void returns_trip_list_if_loggedUser_and_user_are_friends() throws Exception {
-        TripService tripService = new TestableTripService(aLoggedUser);
+        TripService tripService = new TestableTripService(LOGGED_USER);
 
-        User friendUser = anUserWithATripAndFriendOf(aLoggedUser);
-        List<Trip> tripsByUser = tripService.getTripsByUser(friendUser);
+        User friend = anUserWithATripAndFriendOf(LOGGED_USER);
+        List<Trip> friendTrips = tripService.getTripsByUser(friend);
 
-        assertEquals(1, tripsByUser.size());
+        assertEquals(1, friendTrips.size());
     }
 
     private User anUserWithFriends() {
