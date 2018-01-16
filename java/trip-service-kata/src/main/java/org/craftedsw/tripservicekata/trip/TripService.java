dc1b7bda731da7daa541351d14ac62e721d63a2d
Alessio Coser <alessio.coser@xpeppers.com>
2018-01-16 17:23:54 +0100

    uses user.isFriendWith method into TripService

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -17,15 +17,8 @@ public class TripService {
 
 	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
 		List<Trip> tripList = new ArrayList<Trip>();
-		boolean isFriend = false;
 		if (loggedUser != null) {
-			for (User friend : user.getFriends()) {
-				if (friend.equals(loggedUser)) {
-					isFriend = true;
-					break;
-				}
-			}
-			if (isFriend) {
+			if (user.isFriendWith(loggedUser)) {
 				tripList = tripsBy(user);
 			}
 			return tripList;
