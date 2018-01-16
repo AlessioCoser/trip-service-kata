c2628b7c7c29ab6f7e402614cd6aaa5d9f30c85a
Alessio Coser <alessio.coser@xpeppers.com>
2018-01-16 12:09:21 +0100

    extracts getLoggedUser

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,23 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
+import org.junit.Test;
+
 public class TripServiceTest {
-	
+
+    @Test(expected = UserNotLoggedInException.class)
+    public void thows_a_not_logged_in_exception() throws Exception {
+        TripService tripService = new TestableTripService();
+
+        tripService.getTripsByUser(null);
+    }
+}
+
+class TestableTripService extends TripService {
+
+    @Override
+    protected User getLoggedUser() {
+        return null;
+    }
 }
