ff3d040a55b9530622396bbda3c74a47933a3f83
Alessio Coser <alessio.coser@xpeppers.com>
2018-01-16 17:47:56 +0100

    use mockito to mock tripDao

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -3,26 +3,37 @@ package org.craftedsw.tripservicekata.trip;
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.Arrays;
 import java.util.List;
 
 import static junit.framework.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
 public class TripServiceTest {
 
     private static User GUEST = null;
     private static User LOGGED_USER = new User();
     private static User ANOTHER_USER = new User();
 
+    @Mock
+    private TripDAO dao;
+
     @Test(expected = UserNotLoggedInException.class)
     public void thows_a_not_logged_in_exception() throws Exception {
-        TripService tripService = new TestableTripService(GUEST, null);
+        TripService tripService = new TripService(GUEST, dao);
 
         tripService.getTripsByUser(null);
     }
 
     @Test
     public void returns_empty_list_if_user_has_no_friends() throws Exception {
-        TripService tripService = new TestableTripService(LOGGED_USER, null);
+        TripService tripService = new TripService(LOGGED_USER, dao);
 
         List<Trip> friendTrips = tripService.getTripsByUser(ANOTHER_USER);
 
@@ -31,7 +42,7 @@ public class TripServiceTest {
 
     @Test
     public void returns_empty_list_if_loggedUser_and_user_are_not_friends() throws Exception {
-        TripService tripService = new TestableTripService(LOGGED_USER, null);
+        TripService tripService = new TripService(LOGGED_USER, dao);
 
         User friend = anUserWithFriends();
         List<Trip> friendTrips = tripService.getTripsByUser(friend);
@@ -41,12 +52,16 @@ public class TripServiceTest {
 
     @Test
     public void returns_trip_list_if_loggedUser_and_user_are_friends() throws Exception {
-        TripService tripService = new TestableTripService(LOGGED_USER, null);
+        List<Trip> trips = Arrays.asList(new Trip());
+        when(dao.tripsBy(any(User.class))).thenReturn(trips);
+
+        TripService tripService = new TripService(LOGGED_USER, dao);
 
         User friend = anUserWithATripAndFriendOf(LOGGED_USER);
         List<Trip> friendTrips = tripService.getTripsByUser(friend);
 
         assertEquals(1, friendTrips.size());
+        assertEquals(trips, friendTrips);
     }
 
     private User anUserWithFriends() {
@@ -60,16 +75,5 @@ public class TripServiceTest {
         user.addFriend(friend);
         user.addTrip(new Trip());
         return user;
-    }
-
-    private class TestableTripService extends TripService {
-        public TestableTripService(User loggedUser, TripDAO tripDAO) {
-            super(loggedUser, tripDAO);
-        }
-
-        @Override
-        protected List<Trip> tripsBy(User user) {
-            return user.trips();
-        }
     }
 }
