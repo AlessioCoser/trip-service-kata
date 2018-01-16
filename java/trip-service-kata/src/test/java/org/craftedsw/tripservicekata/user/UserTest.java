52418b0800aa366a12b7a9d09b553f0007ae8471
Alessio Coser <alessio.coser@xpeppers.com>
2018-01-16 17:22:58 +0100

    adds isFriendWith method to user

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/user/UserTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/user/UserTest.java
@@ -1,5 +1,27 @@
 package org.craftedsw.tripservicekata.user;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class UserTest {
 
+    private static User STRANGER = new User();
+    private static User FRIEND = new User();
+
+    @Test
+    public void returns_false_if_user_is_not_a_friend() throws Exception {
+        User user = new User();
+
+        assertFalse(user.isFriendWith(STRANGER));
+    }
+
+    @Test
+    public void returns_true_if_user_is_a_friend() throws Exception {
+        User user = new User();
+        user.addFriend(FRIEND);
+
+        assertTrue(user.isFriendWith(FRIEND));
+    }
 }
